ea0d4b80443e7e2c2b3f9f1f43bed6db89969c80
SabariVig <SabariVig@users.noreply.github.com>
2020-12-11 17:36:23 +0000

    deploy: 73a70637c95da14f33d827a86599a641b8df903f

---
+++ precache-manifest.cb43ce4e8e3767b433c69e6ca6c349cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f172a3c592077d4bf3ab2c17cdf76f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b94790efb79b66af670e",
+    "url": "/static/css/main.7777ed0b.chunk.css"
+  },
+  {
+    "revision": "87d287242e45cfafe11c",
+    "url": "/static/js/2.1039e445.chunk.js"
+  },
+  {
+    "revision": "b94790efb79b66af670e",
+    "url": "/static/js/main.b7266572.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7c94dfd7a57df4f472fef80abb140f6e",
+    "url": "/static/media/copy.7c94dfd7.svg"
+  },
+  {
+    "revision": "80a012ba333ef9734647aaf7dd12f3d1",
+    "url": "/static/media/download.80a012ba.svg"
+  },
+  {
+    "revision": "b4045f22673a1570a1f93fefbacc54cf",
+    "url": "/static/media/file.b4045f22.svg"
+  },
+  {
+    "revision": "985c7dffbf994c67d1ecb1292b965fa9",
+    "url": "/static/media/link-c.985c7dff.svg"
+  },
+  {
+    "revision": "df43be104d665913ab8a9cefff984a2d",
+    "url": "/static/media/link.df43be10.svg"
+  }
+]);
